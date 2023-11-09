dcee24049692246d4eff55e69a6c5bbd1a24e6c4
maridesu00 <143613118+maridesu00@users.noreply.github.com>
2023-11-09 11:08:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,15 +11,67 @@ form.addEventListener('submit', addItem);
 ItemList.addEventListener('click' , removeItem);
 filter.addEventListener('keyup', buscarItems);
 
-function buscarItems() {
-    alert('busca alguma coisa neném?');
+function buscarItems(e) {
+    //alert('busca alguma coisa neném?');
+    //entender como o text é coletado letra a letra
+    console.log(e.target.value.toLowerCase());
+//armazenando valor digitado
+    let txt = e.target.value.toLowerCase();
+        //buscar todos os itens
+        let Items = ItemList.getElementsByTagName('li');
+
+    Array.from(Items).forEach( function(item){
+            //busca o item na lista
+        let nomeItem = item.firstChild.textContent;
+    if(nomeItem.toLowerCase().indexOf(txt) != -1){
+        item.style.display = 'block'; //mostra o item
+    } else {
+        item.style.display = 'none'; //oculta o item
+    }
+    });
 }
 
-function removeItem() {
-    alert('clicou na ul!!!');
+function removeItem(evento) {
+    /*console.log('clicou errado! isso nao é um botão!!');*/
+    if(evento.target.classList.contains('delete')) {
+        //console.log('TENHO Q DELETAR');
+        let li = evento.target.parentElement;
+        ItemList.removeChild(li);
+    }
+
 }
 
-function addItem() {
-    alert('você clicou!!!');
+function addItem(evento) {
+evento.preventDefault();
+
+//criar tag <li>
+let novo_li = document.createElement('li');
+//add a classe para tag li class =list-group-item
+novo_li.className = 'list-group-item';
+//resgatamos o valor inserido no formulário
+let novoItem = document.getElementById('item').value;
+
+//unindo o <li> com o nome do item
+novo_li.appendChild(document.createTextNode(novoItem));
+
+//criar o botão deletar
+let deleteBtn = document.createElement('button');
+//atribuindo classes ao btn
+deleteBtn.className = 'btn btn-danger btn-sm float-end delete';
+//acrescentando x ao btn
+deleteBtn.appendChild(document.createTextNode('X'));
+
+
+//colocar o btn dentro da tag <li>
+novo_li.appendChild(deleteBtn)
+
+console.log(novo_li)
+/*console.log(evento) */
+
+// adicionando<li> completa a <ul>
+ItemList.appendChild(novo_li);
+
+//limpar campos input
+form.reset();
 
 }
